ee321669a377a12239f4014bce6c880332dbea00
prescott breeden <prescott.breeden@gmail.com>
2018-05-20 19:47:13 -0700

    styling options in react
    inline as well as importing css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
+// components
 import React, { Component } from 'react';
-import './App.css';
 import Person from './Person/Person';
+
+// css files
+import './App.css';
+import './Person/Person.css';
 
 class App extends Component {
   state = {
@@ -34,10 +38,21 @@ class App extends Component {
   }
 
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      color: 'black',
+      cursor: 'pointer'
+    }
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <button onClick={() => this.switchNameHandler('Bouncy')}>Switch Name</button>
+        <button 
+          style={style}
+          onClick={() => this.switchNameHandler('Bouncy')}>Switch Name</button>
         <Person 
           name={this.state.persons[0].name} 
           age={this.state.persons[0].age}
